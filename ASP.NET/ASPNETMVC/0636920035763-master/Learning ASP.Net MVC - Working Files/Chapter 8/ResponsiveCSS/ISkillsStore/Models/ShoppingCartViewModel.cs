8ee151274ccca5760a271062cb8d81f7eb70b710
DoshiChiragK <doshichiragk@gmail.com>
2026-03-26 19:45:57 -0400

    Removed training material

--- ASP.NET/ASPNETMVC/0636920035763-master/Learning ASP.Net MVC - Working Files/Chapter 8/ResponsiveCSS/ISkillsStore/Models/ShoppingCartViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace ISkillsStore.Models
-{
-    public class ShoppingCartViewModel
-    {
-        public ShoppingCartModel Cart { get; set; }
-        public string ReturnUrl { get; set; }
-    }
-}
